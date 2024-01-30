4425a9523ede8c023a84c0f4bcc29ee902433022
R6SGROUND <xieqiqiang00@live.com>
2024-01-30 08:59:52 +0800

    X

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
+import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-// import './style/index.css';
 import App from './routes/home/index';
 
-// export default ()=>{
-//     return <div>
-//         nmsl
-//     </div>
-// };
+
+class Root extends React.Component {
+  render() {
+    return (
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  }
+}
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
